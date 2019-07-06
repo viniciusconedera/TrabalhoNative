798a47b0834a3eac20b2bc58c85f14fec7000022
viniciusconedera <vinicius_dogma@hotmail.com>
2019-07-06 12:48:24 -0300

    fix: imports on cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,64 +1,80 @@
-import React from 'react';
-import api from '../../../services/api';
-import styled from 'styled-components'
-import { Alert } from 'react-native'
+import React, { Component } from "react";
+import api from "../services/api";
+import styled from "styled-components";
+import { Alert } from "react-native";
 
-export default class cadastroCliente {
+export default class Cadastro extends Component {
+  state = {
+    nome: "",
+    apelido: "",
+    email: "",
+    senha: "",
+    ativo: true
+  };
 
-    state = {
-        nome: "",
-        apelido: "",
-        email: "",
-        senha: "",
-        ativo: true
-    };
+  enviar = () => {
+    api.post("/cliente", this.state).then(function(response) {
+      //Alert.alert(`Clietne ${this.state.nome} Salvo`);
+    });
+  };
 
-    enviar = () => {
-        api.post('/cliente', this.state)
-        .then(function(response){
-            Alert.alert(`Clietne ${this.state.nome} Salvo`);
-        }); 
-    }
-
-    render() {
-        return (
-                <Conteiner>
-                    <Title>Informações Pessoais</Title>
-                    <Label>Nome</Label>
-                    <Input onChangeText={(text) => this.setState({nome: text})} />
-                    <Label>Apelido</Label>
-                    <Input onChangeText={(text) => this.setState({apelido: text})}/>
-                    <Label>E-mail</Label>
-                    <Input onChangeText={(text) => this.setState({email: text})}/>
-                    <Label>Senha</Label>
-                    <Input onChangeText={(text) => this.setState({senha: text})}/>
-                    <BtnEnviar onPress={this.enviar}>
-                        <Label>Entrar</Label>
-                    </BtnEnviar>
-                </Conteiner>
-        )
-    }
+  render() {
+    return (
+      <Conteiner>
+        <Title>Informações Pessoais</Title>
+        <Label>Nome</Label>
+        <Input onChangeText={text => this.setState({ nome: text })} />
+        <Label>Apelido</Label>
+        <Input onChangeText={text => this.setState({ apelido: text })} />
+        <Label>E-mail</Label>
+        <Input onChangeText={text => this.setState({ email: text })} />
+        <Label>Senha</Label>
+        <Input onChangeText={text => this.setState({ senha: text })} />
+        <BtnEnviar onPress={() => this.enviar()}>
+          <BtnLabel>Entrar</BtnLabel>
+        </BtnEnviar>
+      </Conteiner>
+    );
+  }
 }
 
 const Conteiner = styled.View`
   flex: 1;
   background-color: #d9d6e5;
   align-items: center;
-`
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 18px;
+  margin-bottom: 10px;
+  color: #444380;
+`;
+
 const Label = styled.Text`
-    font-size: 16px;
-    font-weight: bold;
-    color: #444380;
-`
-const Imput = styled.TextInput`
-    margin-top: 15px;
-    border: 2px solid #444380;
-    border-radius: 5px;
-    padding: 5px;
-`
-const BtnEnviar = styled.TouchableOpacity`
-  margin-top: 15px;
+  margin-top: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #444380;
+`;
+
+const Input = styled.TextInput`
+  width: 90%;
   border: 2px solid #444380;
   border-radius: 5px;
   padding: 5px;
+`;
+
+const BtnEnviar = styled.TouchableOpacity`
+  margin-top: 30px;
+  border: 2px solid #444380;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const BtnLabel = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #444380;
 `;
