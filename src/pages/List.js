4ebfc93ec21f63adab50d8f0400549eadaf3e154
Vinicius <vinicius_dogma@hotmail.com>
2019-07-15 20:47:52 -0300

    task: desenvolvendo List.js

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 import styled from 'styled-components'
-
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 export default class Main extends Component {
 
@@ -20,21 +19,38 @@ export default class Main extends Component {
         this.setState({cliente: data});
     };
 
+    RemoveCliente = async (id) => {
+        await api.delete(`/cliente/${id}`)
+        this.loadcliente()
+    }
+
     render() {
-        const { cliente } = this.state;
+        const { clientes } = this.state;
         return (
-            <View>
-                {cliente.map(cliente => (
-                    <Container key={cliente.id}>
-                        <Text>Nome: {cliente.nome}</Text>
-                        <Text>Apelido: {cliente.apelido}</Text>
-                        <Text>email: {cliente.email}</Text>
-                    </Container>
-                ))}
-            </View>            
+            <Layout>
+                <FlatList
+                data={clientes}
+                ListEmptyComponent={<EmptyMenssage>Não há tarefas Castradas</EmptyMenssage>}
+                renderItem={({cliente}) => 
+                <Container key={cliente.id}>
+                    <Text>Nome: {cliente.nome}</Text>
+                    <Text>Apelido: {cliente.apelido}</Text>
+                    <Text>email: {cliente.email}</Text>
+                    <BtnDeletar onPress={() => this.RemoveCliente(cliente.id)}>
+                        <Text>Delete</Text>
+                    </BtnDeletar>
+                </Container>} />
+            </Layout>     
         )
     };
 }
+
+const Layout = styled.View`
+    background-color: #d9d6e5;
+    border: 2px solid #444380;
+    align-items: center;
+    justify-content: center;
+`
 
 const Container = styled.View`
   background-color: #d9d6e5;
@@ -42,4 +58,16 @@ const Container = styled.View`
   border: 2px solid #444380;
   border-radius: 5px;
   margin: 10px;
+`;
+
+const BtnDeletar = styled.TouchableOpacity`
+  border: 2px solid #444380;
+  border-radius: 5px;
+  padding: 2px;
+`;
+
+const EmptyMenssage = styled.Text`
+color: #444380;
+align-items: center;
+justify-content: center;
 `;
